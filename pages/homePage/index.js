407355d6031b53fadbf977e53a9cd8e8afbe2c95
SabrinaSeydaliyeva <seydaliyeva01@bk.ru~>
2023-12-13 15:59:20 +0400

    .

--- pages/homePage/index.js
+++ pages/homePage/index.js
@@ -1,12 +1,12 @@
-let btn=document.querySelector(".order-btn")
-btn.addEventListener('mouseout',noText)
-btn.addEventListener('mouseover',yesText)
+let btn = document.querySelector(".order-btn");
+btn.addEventListener("mouseout", noText);
+btn.addEventListener("mouseover", yesText);
 
-function yesText(){
-    btn.style.color="white"
+function yesText() {
+  btn.style.color = "white";
 }
-noText()
-function noText(){
-    btn.style.color="#2f2105"
+noText();
+function noText() {
+  btn.style.color = "#2f2105";
 }
-yesText()
+yesText();
